b2b0b45022c79f7fa6c6d706a1138827863f1fb1
Kriyen Padayachee <kriyenkp@gmail.com>
2014-04-15 17:42:39 +0200

    add

--- learning.cpp
+++ learning.cpp
@@ -16,8 +16,6 @@ const float FPS = 60;
 const int BOUNCER_SIZE = 32;
 const int scrn_W = 640;
 const int scrn_H = 480;
-int pos_x = scrn_W / 2;
-int pos_y = scrn_H / 2;
 
 static ALLEGRO_COLOR red,blue,black,white,green;
 enum KEYS{ UP, DOWN, LEFT, RIGHT };
@@ -29,6 +27,10 @@ int main(void)
 	float bouncer_x = scrn_W / 2.0 - BOUNCER_SIZE / 2.0;
 	float bouncer_y = scrn_H / 2.0 - BOUNCER_SIZE / 2.0;
 	float bouncer_dx = -4.0, bouncer_dy = 4.0;
+	int curFrame = 0;
+	int frameCount = 0;
+	int frameDelay = 5;
+	const int maxFrame = 8;
 	bool done = false;
 	bool keys[4] = { false, false, false, false };
 
@@ -37,6 +39,7 @@ int main(void)
 	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
 	ALLEGRO_TIMER *timer = NULL;
 	ALLEGRO_BITMAP *bouncer = NULL;
+	ALLEGRO_BITMAP *image[maxFrame];
 
 
 	if (!al_init())											//initialize and check Allegro
@@ -88,7 +91,28 @@ int main(void)
 	al_install_keyboard();
 	al_install_mouse();
 
+
+	//Init animated character
+	al_init_image_addon();
+	//Init bitmap
+	image[0] = al_load_bitmap("./images/dragon/fliegt e0000.bmp");
+	image[1] = al_load_bitmap("./images/dragon/fliegt e0001.bmp");
+	image[2] = al_load_bitmap("./images/dragon/fliegt e0002.bmp");
+	image[3] = al_load_bitmap("./images/dragon/fliegt e0003.bmp");
+	image[4] = al_load_bitmap("./images/dragon/fliegt e0004.bmp");
+	image[5] = al_load_bitmap("./images/dragon/fliegt e0005.bmp");
+	image[6] = al_load_bitmap("./images/dragon/fliegt e0006.bmp");
+	image[7] = al_load_bitmap("./images/dragon/fliegt e0007.bmp");
+
+	for (int i = 0; i < maxFrame; i++)
+	{
+		al_convert_mask_to_alpha(image[i], al_map_rgb(106, 76, 48));
+	}
+
+	//end animated character
+
 	//Event queue - register listeners
+	al_register_event_source(event_queue, al_get_timer_event_source(timer));
 	al_register_event_source(event_queue, al_get_keyboard_event_source());
 	al_register_event_source(event_queue, al_get_display_event_source(display));
 	//end event queue
@@ -102,13 +126,14 @@ int main(void)
 	//End Colours
 
 	//End initialisers
-	
+
+	al_start_timer(timer);
 	al_set_target_bitmap(bouncer);
-	al_clear_to_color(green);
+	al_clear_to_color(black);
 	al_set_target_bitmap(al_get_backbuffer(display));
-	al_draw_filled_rectangle(pos_x, pos_y, pos_x + 30, pos_y + 30, green);
+	//al_draw_filled_rectangle(pos_x, pos_y, pos_x + 30, pos_y + 30, green);
 	al_flip_display();
-	//al_start_timer(timer);
+	
 	
 	while (!done)
 	{
@@ -178,6 +203,17 @@ int main(void)
 				break;
 			}
 		}
+		else if (ev.type == ALLEGRO_EVENT_TIMER)
+		{
+			if (++frameCount >= frameDelay)
+			{
+				if (++curFrame >= maxFrame)
+				{
+					curFrame = 0;
+				}
+				frameCount = 0;
+			}
+		}
 		else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
 		{
 			done = true;
@@ -190,13 +226,22 @@ int main(void)
 
 
 
-		al_draw_filled_rectangle(pos_x, pos_y, pos_x + 30, pos_y + 30, green);
+		//al_draw_filled_rectangle(pos_x, pos_y, pos_x + 30, pos_y + 30, green);
+		al_draw_bitmap(image[curFrame], pos_x, pos_y, 0);
 		al_flip_display();
-		al_clear_to_color(al_map_rgb(0, 0, 0));
+		al_clear_to_color(black);
 	}
 
-	al_destroy_event_queue(event_queue);
-	al_destroy_display(display);						//destroy our display object
+	//Destruction
 
+	for (int i = 0; i < maxFrame; i++)
+	{
+		al_destroy_bitmap(image[i]);
+	}
+	al_destroy_bitmap(bouncer);
+	al_destroy_event_queue(event_queue);
+	al_destroy_display(display);
+	al_destroy_timer(timer);
+	//end Distruction
 	return 0;
 }
