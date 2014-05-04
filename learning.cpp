24e31b4700f8252c363c1e4edfa6eb6ef573becb
Rambo <rambo@rambo.co.za>
2014-05-04 18:15:17 +0200

    Some tweaks and file cleanup

--- learning.cpp
+++ learning.cpp
@@ -7,7 +7,7 @@
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>
 #include <allegro5/allegro_audio.h>
-#include <objects.h>
+#include <lib/objects.h>
 #include <cstdio>
 
 using namespace std;
