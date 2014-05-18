86f93e90ab1c41db5fa97e8d0e734708bf67834b
Sulaiman S Patel <sulaimansaleempatel@yahoo.co.uk>
2014-05-18 19:11:50 +0200

    Another Commit (hoping)

---
+++ Gameplay.cpp
@@ -0,0 +1,15 @@
+/*
+*	This class contains code to manage how GameElements interact with each other.
+*/
+
+#include "Graphics_and_Animations.cpp"
+
+class Gameplay{
+public:
+	bool detectCollision(DynamicImg[], DynamicImg[]);
+};
+
+bool Gameplay::detectCollision(DynamicImg images1[], DynamicImg images2[])
+{
+	return true;
+}
